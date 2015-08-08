bd41b7928577df58392bffd192b29319a5e11323
roof <roof@odd-e.com>
2015-08-08 14:20:57 +0700

    Operand class

--- captcha.py
+++ captcha.py
@@ -5,7 +5,6 @@ class Captcha:
 		self.operator = operator
 		self.rightOperand = rightOperand
 		self.map_number = {1:"one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six", 7:"seven", 8:"eight", 9:"nine", 0:"zero"}
-		self.map_operator = {1:"+", 2:"*", 3:"-"}
 
 	def left_operand(self):
 		if self.pattern == 2:
@@ -29,3 +28,14 @@ class Operator:
 
 	def toString(self):
 		return self.map_operator[self.operator]
+
+class Operand:
+	def __init__(self,pattern,operand):
+		self.pattern = pattern
+		self.operand = operand
+		self.map_number = {1:"one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six", 7:"seven", 8:"eight", 9:"nine", 0:"zero"}
+
+	def toString(self):
+		if self.pattern == 2:
+			return self.map_number[self.operand]
+		return str(self.operand)
--- test_captcha.py
+++ test_captcha.py
@@ -1,6 +1,7 @@
 import unittest
 from captcha import Captcha
 from captcha import Operator
+from captcha import Operand
 
 class TestCaptchaFirstPatternLeftOperand(unittest.TestCase):
 	firstPattern = 1
@@ -86,4 +87,21 @@ class TestOperator(unittest.TestCase):
 		operator = Operator(1)
 		self.assertEqual('+', operator.toString())
 
+	def test_2_should_be_multiply(self):
+		operator = Operator(2)
+		self.assertEqual('*', operator.toString())
+
+	def test_3_should_be_multiply(self):
+		operator = Operator(3)
+		self.assertEqual('-', operator.toString())
+
+
+class TestOperand(unittest.TestCase):
+	def test_first_pattern_1_should_be_1(self):
+		operand = Operand(1,1)
+		self.assertEqual('1', operand.toString())
+
+	def test_second_pattern_1_should_be_one(self):
+		operand = Operand(2,1)
+		self.assertEqual('one', operand.toString())
 
