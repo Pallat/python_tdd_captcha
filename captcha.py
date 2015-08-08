de867bb3de918b4413fac1472d3495524dc9d17d
roof <roof@odd-e.com>
2015-08-08 14:22:47 +0700

    class captcha use operand class

--- captcha.py
+++ captcha.py
@@ -7,14 +7,12 @@ class Captcha:
 		self.map_number = {1:"one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six", 7:"seven", 8:"eight", 9:"nine", 0:"zero"}
 
 	def left_operand(self):
-		if self.pattern == 2:
-			return self.map_number[self.leftOperand]
-		return str(self.leftOperand)
+		operandObj = Operand(self.pattern, self.leftOperand)
+		return operandObj.toString()
 
 	def right_operand(self):
-		if self.pattern == 2:
-			return self.map_number[self.rightOperand]
-		return str(self.rightOperand)
+		operandObj = Operand(self.pattern, self.rightOperand)
+		return operandObj.toString()
 
 	def operator_flag(self):
 		operObj = Operator(self.operator)
