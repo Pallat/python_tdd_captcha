882ec80af06a410907999737e7b27607ec08815f
Twin Panichsombat <roofimon@gmail.com>
2015-08-13 09:39:29 +0700

    Remove fizzbuzz

--- test_app.py
+++ test_app.py
@@ -7,32 +7,7 @@ class TestFizzBuzzAPI(unittest.TestCase):
 
 	def test_root(self):
 		response = self.app.get('/')
-		self.assertEqual(200, response.status_code)
-		self.assertEqual('Hello World', response.data)
-
-	def test_fizzbuzz_return_default(self):
-		response = self.app.get('/fizzbuzz/1')
-		self.assertEqual('1', response.data)
-		response = self.app.get('/fizzbuzz/4')
-		self.assertEqual('4', response.data)
-
-	def test_fizzbuzz_return_fizz(self):
-		response = self.app.get('/fizzbuzz/3')
-		self.assertEqual('fizz', response.data)
-		response = self.app.get('/fizzbuzz/6')
-		self.assertEqual('fizz', response.data)
-
-	def test_fizzbuzz_return_buzz(self):
-		response = self.app.get('/fizzbuzz/5')
-		self.assertEqual('buzz', response.data)
-		response = self.app.get('/fizzbuzz/10')
-		self.assertEqual('buzz', response.data)
-
-	def test_fizzbuzz_return_fizzbuzz(self):
-		response = self.app.get('/fizzbuzz/15')
-		self.assertEqual('fizzbuzz', response.data)
-		response = self.app.get('/fizzbuzz/30')
-		self.assertEqual('fizzbuzz', response.data)
+		self.assertEqual(404, response.status_code)
 
 	def test_route_captcha(self):
 		response = self.app.get('/captcha')
